4068bcd82778aefef4606db4c9be7e81ec906f8f
Artur Moises <arturmoiscontato@gmail.com>
2025-05-11 15:07:49 -0300

    update: server.js to use environment variable for port configuration and change log message to Portuguese

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
